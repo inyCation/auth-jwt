113a6a64808c538555e58e622d721d09075b16e3
Deepak Raj <3razz@gmail.com>
2024-03-05 17:12:54 +0530

    added link to app.jsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const App :React.FC= () => {
   return (
     <div className='userLoginContainer' > 
-        test
+      <Link to="/login">Login</Link>
+      <Link to="/register">register</Link>
+      <Link to="/post">post</Link>
     </div>
   )
 }
